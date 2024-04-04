77919c324342185fffc12ecaa5b70578820dc8ac
njhang <njhang1@snu.ac.kr>
2024-04-04 16:11:58 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,21 @@
+
+
 def main():
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    if y == 0:
+        print("Error: canoot divide by zero.")
+    else:
+        return print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+          
+def add(a, b):
+    return a + b
+           
+def divide(x,y):
+    return x/y
 
-def add():
-
-def divide():
+if __name__ == "__main__":
+    main()
